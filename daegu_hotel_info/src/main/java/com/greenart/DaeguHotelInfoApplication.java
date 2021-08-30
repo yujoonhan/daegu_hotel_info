8eb2087e5c30ca0f82b17b73d785a00641526674
yujoonhan <sick1925@gmail.com>
2021-08-30 11:47:30 +0900

    대구 호텔 정보

---
+++ daegu_hotel_info/src/main/java/com/greenart/DaeguHotelInfoApplication.java
@@ -0,0 +1,13 @@
+package com.greenart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeguHotelInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaeguHotelInfoApplication.class, args);
+	}
+
+}
---
+++ daegu_hotel_info/src/test/java/com/greenart/DaeguHotelInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaeguHotelInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
